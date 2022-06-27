5588bb17c05d5831195967b88304474bf7a221b8
DESKTOP-RR88DN6\05276 <tamar.lando@ideo-digital.com>
2022-06-27 21:51:23 +0300

    continue

--- src/app/@shared/configuration/tag-form/tag-form.service.ts
+++ src/app/@shared/configuration/tag-form/tag-form.service.ts
@@ -1,17 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BaseHttpService } from '@app/@core/base/base-http-service';
 import { DynamicFormControl } from '@app/@forms/@core/interfaces/dynamic-form-control';
 import { FormTextComponent } from '@app/@forms/form-fields/form-text/form-text.component';
+import { QueryBuilderService } from '@app/@ideo/infrastructure/services/query-builder.service';
 import { TagModel } from '../../models/tag.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TagFormService {
+export class TagFormService extends BaseHttpService<TagModel> {
+  public collationName: string = "Tags";
 
-  constructor() { }
+  constructor(_http: HttpClient, _queryBuilder: QueryBuilderService) {
+    super(_http, _queryBuilder);
+  }
   generate(): DynamicFormControl<TagModel>[] {
     const form: DynamicFormControl<TagModel>[] = [];
-
     form.push(
       {
         type: FormTextComponent,
@@ -34,4 +39,6 @@ export class TagFormService {
     );
     return form;
   }
+
+
 }
