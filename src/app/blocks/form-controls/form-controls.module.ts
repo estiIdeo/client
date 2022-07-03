1d10b8b81bd91393124bbd25598e17ba6f0c8466
DESKTOP-RR88DN6\05276 <tamar.lando@ideo-digital.com>
2022-07-03 15:36:10 +0300

    downgrage version 12

--- src/app/blocks/form-controls/form-controls.module.ts
+++ src/app/blocks/form-controls/form-controls.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { IconsModule } from '../icons/icons.module';
+
 import { ButtonComponent } from './button/button.component';
 import { ButtonLanguageSelectorComponent } from './button-language-selector/button-language-selector.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { CardsModule } from '../cards/cards.module';
 import { CheckboxComponent } from './checkbox/checkbox/checkbox.component';
 
+
 const exports = [
+  CheckboxComponent,
   ButtonComponent,
-  ButtonLanguageSelectorComponent
+  ButtonLanguageSelectorComponent,
 ];
 
-const exportsModule = [FormsModule, ReactiveFormsModule,CheckboxComponent,ButtonComponent,ButtonLanguageSelectorComponent];
+const exportsModule = [FormsModule, ReactiveFormsModule];
 
 @NgModule({
   declarations: [...exports],
-  imports: [CommonModule, BsDropdownModule,FontAwesomeModule, ...exportsModule],
+  imports: [CommonModule, IconsModule, BsDropdownModule, TranslateModule, CardsModule, ...exportsModule],
   exports: [...exports, ...exportsModule],
 })
 export class FormControlsModule {}
